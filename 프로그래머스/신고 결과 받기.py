fa4c4857360beadcf84dea3fd280d2a51ceab7b0
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-06 00:45:31 +0900

    추가: 제일 작은 수 제거하기

---
+++ 프로그래머스/신고 결과 받기.py
@@ -0,0 +1,9 @@
+"""
+* Title: 신고 결과 받기
+https://school.programmers.co.kr/learn/courses/30/lessons/92334
+
+* Input
+
+* Output
+
+"""
--- 프로그래머스/제일 작은 수 제거하기.py
+++ 프로그래머스/제일 작은 수 제거하기.py
@@ -1,5 +1,6 @@
 """
 * Title: 제일 작은 수 제거하기
+https://school.programmers.co.kr/learn/courses/30/lessons/12935
 
 * Input
 4 3 2 1
