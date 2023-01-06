d593395b8df0fb2ae6c60688d3f7d98dbade4298
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-06 23:58:54 +0900

    230106 연습

---
+++ _연습장/202301/06_금/한 줄 입력받아 출력.py
@@ -0,0 +1,15 @@
+"""
+* Title: 한 줄 입력받아 출력
+
+* Input
+Hello, Coding Test!
+
+* Output
+Hello, Coding Test!
+"""
+import sys
+# 하나의 문자열 데이터 입력받기
+input_data = sys.stdin.readline().rstrip()
+
+# 입력받은 문자열 그대로 출력
+print(input_data)
