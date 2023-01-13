1a2a3a658736412d60e304669778c3a9fd287119
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-13 23:51:21 +0900

    230113 연습

---
+++ 연습장/202301/13_금/자연수 뒤집어 배열로 만들기.py
@@ -0,0 +1,6 @@
+def solution(n):
+    arr = list(str(n))
+    arr.reverse()
+    answer = list(map(int, arr))
+
+    return answer
---
+++ 연습장/202301/13_금/평균 구하기.py
@@ -0,0 +1,5 @@
+def solution(arr):
+    result = sum(arr)
+    answer = result / int(len(arr))
+
+    return answer
