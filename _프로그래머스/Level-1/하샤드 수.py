bceb84eab9a7d105bce0a05c48aaddcdc4d907af
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-15 23:51:22 +0900

    추가) 하샤드 수, 문자열을 정수로 바꾸기

---
+++ _프로그래머스/Level-1/하샤드 수.py
@@ -0,0 +1,17 @@
+"""
+* Title: 하샤드 수
+https://school.programmers.co.kr/learn/courses/30/lessons/12947
+"""
+def solution(x):
+    arr = list(str(x))
+    sum = 0
+    answer = True
+
+    for i in range(len(arr)):
+        sum += int(arr[i])
+        if x % sum == 0:
+            answer = True
+        else:
+            answer = False
+
+    return answer
