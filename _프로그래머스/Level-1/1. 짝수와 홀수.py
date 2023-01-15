b1aa9de201673a7cd978f8925efb0072beac54c6
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-16 00:15:05 +0900

    추가) 정수 내림차순으로 배치하기, 두 정수 사이의 합

---
+++ _프로그래머스/Level-1/1. 짝수와 홀수.py
@@ -0,0 +1,13 @@
+"""
+* Title: 짝수와 홀수
+https://school.programmers.co.kr/learn/courses/30/lessons/12937
+"""
+def solution(num):
+    answer = ''
+
+    if (num % 2 == 0):
+        answer = 'Even'
+    else:
+        answer = 'Odd'
+
+    return answer
