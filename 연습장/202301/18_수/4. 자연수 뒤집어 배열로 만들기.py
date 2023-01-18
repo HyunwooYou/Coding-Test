c0607c372a67ef6675d18187589af3ce2197f051
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-18 22:00:50 +0900

    230118 연습

---
+++ 연습장/202301/18_수/4. 자연수 뒤집어 배열로 만들기.py
@@ -0,0 +1,6 @@
+def solution(n):
+    array = list(str(n))
+    array.reverse()
+    answer = list(map(int, array))
+
+    return answer
--- 연습장/202301/18_수/8. 하샤드 수.py
+++ 연습장/202301/18_수/8. 하샤드 수.py
@@ -1,18 +1,14 @@
-"""
-* Title: 하샤드 수
-https://school.programmers.co.kr/learn/courses/30/lessons/12947
-"""
 def solution(x):
     arr = list(str(x))
     sum = 0
     answer = True
 
     for i in range(len(arr)):
-        sum += int(arr[i])
+        sum = sum + int(arr[i])
 
     if x % sum == 0:
         answer = True
     else:
-        answer = False
+        answer= False
 
     return answer
