cdab85711c73c97a360662d07777dbf188cab909
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-20 16:28:06 +0900

    추가) 최댓값과 최솟값
    strls = s.split()
    intls.append(int(strls[i]))
    min = str(intls[0])

--- _프로그래머스/19. 문자열 다루기 기본.py
+++ _프로그래머스/19. 문자열 다루기 기본.py
@@ -3,14 +3,13 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/12918
 """
 def solution(s):
-    answer = True
     length = len(s)
 
     if not (length == 4 or length == 6):
-        answer = False
+        return False
     if i in range(0, length):
         num = ord(s[i]) - 48
         if num >= 10:
-            answer = False
+            return False
 
-    return answer
+    return True
