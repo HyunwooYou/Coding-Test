192cd46f49a628e0b18f8ef8c14b24238a569146
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-21 16:52:42 +0900

    추가) 행렬의 덧셈

---
+++ _프로그래머스/22. 행렬의 덧셈.py
@@ -0,0 +1,14 @@
+"""
+* Title: 행렬의 덧셈
+
+"""
+def solution(arr1, arr2):
+    answer = []
+
+    for i in range(len(arr1)):
+        new_arr = []
+        for j in range(len(arr1[i])):
+            new_arr.append(arr1[i][j] + arr2[i][j])
+        answer.append(new_arr)
+
+    return answer
