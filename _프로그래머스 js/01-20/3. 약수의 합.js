be16b9fd458ffd9e194312994f454d63e57fc834
gachonhyun <gusdn1460@nate.com>
2023-08-19 17:46:15 +0900

    feat: python to js

---
+++ _프로그래머스 js/01-20/3. 약수의 합.js
@@ -0,0 +1,17 @@
+/*
+* Title: 약수의 합
+https://school.programmers.co.kr/learn/courses/30/lessons/12928
+*/
+const solution = (num) => {
+  let answer = 0;
+  
+  for (let i = 1; i <= num; i ++) {
+    if (num % i === 0) {
+      answer += i;
+    }
+  }
+  
+  return answer;
+}
+
+console.log(solution(10))
