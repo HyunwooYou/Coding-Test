87cb416a4be1848a2aa02621598cb15f2a973d2f
hyunwoo <gusdn1460@nate.com>
2023-11-10 17:14:57 +0900

    Update 최댓값과 최솟값.js

---
+++ Practice Program/70 percent/최댓값과 최솟값.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  const ls = s.split(' ');
+  const min = Math.min(...ls);
+  const max = Math.max(...ls);
+
+  return `${min} ${max}`;
+}
+
+s = '1 2 3 4';
+console.log(solution(s));
+// 1 2 3 4
