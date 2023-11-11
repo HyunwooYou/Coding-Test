5b05ac63ea83e288a212e66b10ba1c93cdaf6576
hyunwoo <gusdn1460@nate.com>
2023-11-11 13:36:49 +0900

    Create 최솟값 만들기.js

---
+++ 연습문제/70 percent/최솟값 만들기.js
@@ -0,0 +1,13 @@
+function solution(A, B) {
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  const multipliedLs = A.map((el, idx) => el * B[idx]);
+  const sumOfLs = multipliedLs.reduce((acc, cur) => acc + cur, 0);
+
+  return sumOfLs;
+}
+
+A = [1, 4, 2];
+B = [5, 4, 4];
+console.log(solution(A, B));
