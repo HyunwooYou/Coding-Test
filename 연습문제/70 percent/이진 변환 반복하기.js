dfbba5aec7a570271cd28918e11404fc5ab61964
hyunwoo <gusdn1460@nate.com>
2023-11-12 12:08:18 +0900

    Create 이진 변환 반복하기.js

---
+++ 연습문제/70 percent/이진 변환 반복하기.js
@@ -0,0 +1,32 @@
+/*
+#console #가지런히 #일정하게 출력
+console.log(`${s}\t${newStr}\t${len}`);
+
+#이진 변환
+const len = 10
+const trans = len.toString(2); 
+ */
+
+function solution(s) {
+    let varObj = { str: '', len: 0 };
+    let cntObj = { zero: 0, change: 0 };
+
+    while (true) {
+        if (varObj.str === '1') {
+            break;
+        }
+
+        cntObj.zero += s.split('').filter((e) => e === '0').length;
+        cntObj.change += 1;
+
+        varObj.str = s.replace(/0/g, '');
+        varObj.len = varObj.str.length;
+
+        s = varObj.len.toString(2);
+    }
+
+    return [cntObj.change, cntObj.zero];
+}
+
+s = '110010101001';
+console.log(solution(s));
