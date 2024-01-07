846f75e929e9bc14cf4bf4b0bd8b27fdeda67750
hyunwoo <gusdn1460@nate.com>
2024-01-07 19:10:43 +0900

    Create 멀리 뛰기.js

---
+++ 연습문제/65 percent/멀리 뛰기.js
@@ -0,0 +1,34 @@
+/*
+n = 4
+1) a a a a
+3) b a a
+1) b b
+> 5
+
+n = 5
+1) a a a a a
+4) b a a a
+3) b b a
+> 8
+
+n = 6
+1) a a a a a a
+5) b a a a a
+6) b b a a
+1) b b b
+> 13
+
+ */
+function solution(n) {
+    let cnt = 0;
+
+    //
+    for (let two = 0; two <= n / 2; two++) {
+        cnt += console.log(i);
+    }
+
+    return 0;
+}
+
+n = 4;
+console.log(solution(n));
